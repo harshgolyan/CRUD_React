ac8c5185e1a436e7c6a74db3d14e58fe76ed5882
Harsh Golyan <harshgolyan308@gmail.com>
2024-03-08 10:24:53 +0530

    update cors

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,20 @@ const { MONGOURI } = require('./keys');
 const cors = require('cors')
 const app = express();
 
-app.use(cors())
+app.use(
+    cors({
+      origin: "*",
+      methods: ["GET", "POST", "DELETE", "UPDATE", "PUT"],
+      credentials: true,
+    })
+  );
 app.use(express.json())
 app.use(require('./routes/formRoute'))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 
 
